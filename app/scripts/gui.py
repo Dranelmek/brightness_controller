872839b382803d8a4588c3d3255dd3e6d2d07808
Lenard <bebobhd@gmail.com>
2024-08-05 01:04:31 +0100

    added frame and unpacking function

--- app/scripts/gui.py
+++ app/scripts/gui.py
@@ -8,8 +8,11 @@ root.geometry("400x200")
 root.title("Screen Brightness")
 root.resizable(height=False, width=False)
 
+# section
+sec = tk.Frame(root, padx=0, pady=0)
+
 # app label
-label = tk.Label(root, text="Brightness", font=('Franklin Gothic Heavy', 18))
+label = tk.Label(sec, text="Brightness", font=('Franklin Gothic Heavy', 18))
 label.pack(padx=20, pady=10)
 
 # slider function
@@ -18,14 +21,23 @@ def update(*args):
 
 # brightness slider
 val = tk.DoubleVar()
-slider = tk.Scale(root, variable=val, from_=1, to=100,orient=tk.HORIZONTAL, font=('consolas', 8), length=380, width=30, command=update)
+slider = tk.Scale(sec, variable=val, from_=1, to=100,orient=tk.HORIZONTAL, font=('consolas', 8), length=380, width=30, command=update)
 
 
 # main monitor brightness
 brightness = controller.get(GetOptions.SIMPLE)
-
 slider.set(brightness)
 
+def hide_widget(widget):
+    # This will remove the widget from toplevel
+    widget.pack_forget()
+
+
+def show_widget(widget):
+    # This will recover the widget from toplevel
+    widget.pack()
+
 slider.pack()
+sec.pack()
 
 root.mainloop()
